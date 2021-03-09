841fe8990986ec9e6a265b2088a8504317b2fef0
Lukas Kristiansen <73884104+lukaskrist@users.noreply.github.com>
2021-03-09 11:46:28 +0100

    Initial commit

---
+++ ARS_benchmark.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 10:58:00 2021
+
+@author: loklu
+"""
+
+
+import numpy as np
+import time
+import benchmark_class
+
+
+def MaxFunc(M):
+    maxval = 4
+    for j in M:
+        if j>maxval:
+            j = maxval
+        if j<-maxval:
+            j = -maxval
+    return M
+def softmax(x):
+    """
+    Compute softmax values for each sets of scores in x.
+    """
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+class ARSTrainer():
+
+    def __init__(self):
+        #print("ARS-Wave initialised")
+        #initialize global parameters
+        self.N = 20
+        
+        
+    def train(self,pulse,N,T,alpha,v,maxepochs = 20,data = None,A = np.zeros(1), Noise = None,L = None):
+        """
+        Implement Basic random search
+        psi = our start configuration of the wave function, will be updated along the way
+        psi_t = the target function (will start with going from 0 state to 1 state, but will )
+        u0 = the starting control vector, which will be updated. 
+        alpha = step-size 
+        N = number of directions sampled per iteration 
+        v = standard deviation of the exploration noise
+        p = number of horizons
+        maxepochs = the maximum amount of epochs that we go through before converging
+        theta = the update vector for the u0
+        if we have data we put that in the data, but will first be implemented later, for now that is just none
+        """
+        if L != None:
+            sp = benchmark_class.SpinChain(N,T,L,Noise)
+        else:
+            sp = benchmark_class.TwoLevel(N,T,Noise)
+        ### assertions
+        #assert psi0.size == psi_target.size
+        ### initialize
+        epoch = 0
+        p = 2*N
+        AccHist = []
+        #d = len(psi0)
+        M = pulse #np.zeros((l,N))
+        ### main loop
+        t0 = time.time()
+        times = []
+#        if data != 0:
+#            thing = data
+#            thing[0] = 1
+        #if version = Waveless - Spin chain
+        if A.all() == 0:
+            while epoch < maxepochs:
+                epoch += 1
+
+                samples = np.random.normal(size = (p,N))
+                r_plus_list = []
+                r_minus_list = []
+                M_update = np.zeros((N))
+                for i in range(p):
+                    delta_plus = M+samples[i,:]*v
+                    delta_plus = MaxFunc(delta_plus)
+                    delta_minus = M-samples[i,:]*v
+                    delta_minus = MaxFunc(delta_minus)
+                    r_plus = sp.roll_out(delta_plus)
+                    r_minus = sp.roll_out(delta_plus)
+                    r_plus_list.append(r_plus)
+                    r_minus_list.append(r_minus)
+                    M_update += alpha/N *(r_plus-r_minus )*samples[i,:]
+                    #print(M_update)
+                
+                # update by choosing the standard deviation
+
+                std = np.std([r_plus_list,r_minus_list])
+                if std != 0:
+                    M_update /= std
+
+                M += M_update
+
+                # this can be omitted, but is good for this specific issue. 
+                AccHist.append(np.max([r_plus_list,r_minus_list]))
+                times.append(time.time()-t0)
+                ### END CODE
+        if A.any != 0:
+            delta_A = np.zeros(N)
+            #print(dis_var)
+            delta_B = np.zeros(N)
+            L = len(pulse)
+            while epoch < maxepochs:
+                epoch += 1
+
+                samples = np.random.normal(size = (p,L,N))
+                r_plus_list = []
+                r_minus_list = []
+                M_update = np.zeros((L,N))
+                for i in range(p):
+                    delta_plus = M+samples[i,:,:]*v
+                    delta_minus = M-samples[i,:,:]*v
+                    #delta_plus = MaxFunc(delta_plus)
+                    #delta_minus = MaxFunc(delta_minus)
+                    probs_plus = softmax(delta_plus)
+                    probs_minus = softmax(delta_minus)
+                    for k in range(N):    
+                        delta_A[k] = np.random.choice(A,p=probs_plus[:,k])
+                        delta_B[k] = np.random.choice(A,p=probs_minus[:,k])
+                    r_plus = sp.roll_out(delta_A)
+                    r_minus = sp.roll_out(delta_B)
+                    r_plus_list.append(r_plus)
+                    r_minus_list.append(r_minus)
+                    M_update += alpha/N *(r_plus-r_minus )*samples[i,:,:]
+                
+                # update the 
+                std = np.std([r_plus_list,r_minus_list])
+                #print(std)
+                if std != 0:
+                    M_update /= std
+                M += M_update
+                AccHist.append(np.max([r_plus_list,r_minus_list]))
+                times.append(time.time()-t0)        
+        return AccHist,times
---
+++ test0.py
@@ -0,0 +1,51 @@
+import numpy as np
+import benchmark_class
+import ARS_benchmark
+import matplotlib.pyplot as plt
+N = 5
+T = 1.03
+
+noise = 0.0 # remember noise is optional
+
+two_level = benchmark_class.TwoLevel(N, T, noise)
+
+alist = 2*(np.random.rand(N)-0.5)
+
+# this is how you evaluate it, with step-per-step updates
+
+#for step in range(0, N):
+    
+#    _, r = two_level.update(alist[step])
+#    print("r:", r)
+
+# this is how you use roll_out
+
+G = two_level.roll_out(alist)
+#print("G:", G)
+Tlist = []
+# Note, if the noise is different from zero, you get a different result each time. 
+pulse = np.random.rand(N)
+alpha = 0.1
+v = 0.5
+maxite = 40
+maxepochs = 20
+f_story = np.zeros((maxepochs,maxite))
+times = np.zeros((maxepochs,maxite))
+for i in range(maxite):
+    T += 0.1
+    ARS = ARS_benchmark.ARSTrainer()
+    f_story[:,i],times[:,i] = ARS.train(pulse,N,T,alpha,v,L=5, Noise = noise)
+    Tlist.append(T)
+    
+time_average_A = np.average(times,axis = 1)
+f_story_avg_A = np.average(f_story,axis = 1)
+f_story_std_A = np.std(f_story,axis = 1)
+f_story_max_A = np.max(f_story,axis = 1)
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.plot(Tlist,np.max(f_story,axis = 0),'*',label = "Fidelity max Waveless")
+ax1.set_xlabel('T ')
+ax1.set_ylabel('Fidelity')
+ax1.set_title('QSL ARS graph-Max')
+ax1.legend()
+ax1.set_title('QSL ARS graph')
